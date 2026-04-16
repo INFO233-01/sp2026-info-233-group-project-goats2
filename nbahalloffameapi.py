04a0b7cb5cb692bf06b7f62029385dc9cfe5702c
tjstrick2 <tstrick1@ramapo.edu>
2026-04-15 21:55:38 -0400

    Update nbahalloffameapi.py

--- nbahalloffameapi.py
+++ nbahalloffameapi.py
@@ -20,6 +20,41 @@ response = requests.get(
 # Print the results
 data = response.json()
 print(data)
+data = response.json()
+
+for player in data["data"]:
+    print(f"Name: {player['first_name']} {player['last_name']}")
+    print(f"Position: {player['position']}")
+    print(f"Team: {player['team']['full_name']}")
+    print("-" * 30)
+    
+    response = requests.get(
+    f"{BASE_URL}/v1/stats",
+    headers=headers,
+    params={"per_page": 10}
+)
+
+data = response.json()
+
+for stat in data["data"]:
+    player = stat["player"]
+    
+    print(f"{player['first_name']} {player['last_name']}")
+    print(f"Points: {stat['pts']}")
+    print(f"Rebounds: {stat['reb']}")
+    print(f"Assists: {stat['ast']}")
+    print("-" * 30)
+    player_name = input("Enter player name: ").lower()
+
+for stat in data["data"]:
+    player = stat["player"]
+    full_name = f"{player['first_name']} {player['last_name']}".lower()
+    
+    if player_name in full_name:
+        print(f"{full_name.title()}")
+        print(f"Points: {stat['pts']}")
+        print(f"Rebounds: {stat['reb']}")
+        print(f"Assists: {stat['ast']}")
 
 
 
